d49350e596979fe613fd5e8ce8a8881f7bac3e78
cemilker <c.karaduman@palmet.com>
2022-12-06 09:21:09 +0300

    Yazdırma çalışmaları-1

--- resources/views/directory.blade.php
+++ resources/views/directory.blade.php
@@ -5,7 +5,13 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
+<style type="text/css">
+body {
+-moz-transform: scale(0.9, 0.9); /* Moz-browsers */
+zoom: 0.9; /* Other non-webkit browsers */
+zoom: 90%; /* Webkit browsers */
+}
+</style>
 </head>
 <body>
 
@@ -13,9 +19,14 @@
 
 <div class="container">
   <div class="row">
-    <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
+    <div class="col"><input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.."></div>
+    <div class="col"><button class="btn btn-primary" id="btn1">Yazdır</button></div>
   </div>
 
+<button class="btn btn-primary" id="btn2" onclick="window.print()">Yazdır_</button>
+  
+
+<div id="printarea" class="yazdir">
 
 <table style='width:100%; margin-left:20px; margin-top:30px; table-layout: fixed;' class='table table-bordered' >
 <div class="container">
@@ -50,10 +61,34 @@
 </table>
 </div>
 </div>
+</div>
+
 
 <script>
+    
+        const btn1=document.querySelector("#btn1");
+        const btn2=document.querySelector("#btn2");
+    
+        function yazdir(alanID)
+        {
+            //yazdırılacak nesneyi seçme
+            let yazilacakAlan = document.querySelector(alanID);
+ 
+            //yeni pencere aç, yazdırma alanını ekle
+            //yazdır ve pencereyi kapat
+            pencere=window.open();
+            pencere.document.body.innerHTML=yazilacakAlan.innerHTML; 
+            pencere.print();
+            pencere.close();
+        }
+        
+        //buton tıklama olayları
+        btn1.onclick=function(){
+           yazdir("#printarea");
+        }
+        
+    </script>
 
-</script>
 
 </body>
 </html>
